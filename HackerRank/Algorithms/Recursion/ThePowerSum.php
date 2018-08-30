8cc0afde14f587460dffcf4fed50b35376a2fff0
Imad Chabounia <jokerdz44@gmail.com>
2018-08-30 17:35:07 +0100

    Update ThePowerSum.php
    Solution of HackerRank Problem (The Power Sum) with PHP , i found it a good example of recursion inside a for loop

--- HackerRank/Algorithms/Recursion/ThePowerSum.php
+++ HackerRank/Algorithms/Recursion/ThePowerSum.php
@@ -1,2 +1,28 @@
 <?php
+// additional basic algorithm (compute power of number with for loop) :
 
+
+function power_n($X, $N){
+  
+  $p = 1;
+  
+  for($i = 1; $i <= $N; $i++){
+    $p = $p*$X;
+  }
+  
+  return $p;
+}
+
+
+function powerSumHelper($X, $N, $l){
+  $counter = 0;
+  if($X == 0){ // base case/Condition
+    return 1;
+  }else{
+    for($i = $l + 1; power_n($i, $N) <= $X; $i++){
+       $x = $X - power_n($i, $N);
+       $counter += powerSumHelper($x, $N, $i);
+    }
+  }
+  return $counter;
+}
