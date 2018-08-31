845ad29d9f8e2e8ea01cb50f4cca66f92220cb29
Imad Chabounia <jokerdz44@gmail.com>
2018-08-31 12:52:28 +0200

    Update InsertionSort.php

--- Algorithms/Sorting/InsertionSort.php
+++ Algorithms/Sorting/InsertionSort.php
@@ -1,5 +1,26 @@
 <?php
 
-function InsertionSort($array, $n): array {
+function InsertionSort($array, $n): array { // $n => size of the array
+  
+  if(!(empty($array))){
+    
+    // main Algorithm: 
+    
+    for($i = 1; $i < $n; $i++){
+      $shot = $array[$i];
+      $j = $i-1;
+      
+      while($j >= 0 && $array[$j] > $shot){
+        $array[$j+1] = $array[$j];
+        $j = $j - 1;
+      }
+      
+      $array[$j+1] = $shot;
+        
+    }
+    
+    return $array;
+  
+  }
 
 }
