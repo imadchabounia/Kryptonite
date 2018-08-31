8e8f7f6dd34e54a72c46f14759484747848f171e
Imad Chabounia <jokerdz44@gmail.com>
2018-08-31 17:35:39 +0200

    Update BubbleSort.php

--- Algorithms/Sorting/BubbleSort.php
+++ Algorithms/Sorting/BubbleSort.php
@@ -1,2 +1,44 @@
 <?php 
 
+function swap(&$a, &$b){
+  
+  $temp = $a;
+ 
+  $a = $b;
+  
+  $b = $temp;
+
+}
+
+
+function BubbleSort($array, $n): array  {
+  
+  while(true){
+  
+    $swapped = 0;
+    
+    for($i = 0; $i < $n-1; $i++){
+     
+      if($array[$i+1] < $array[$i]){
+        
+        swap($array[$i], $array[$i+1]);
+        $swapped = 1;
+     
+      }
+    }
+    
+    if($swapped == 0){
+      
+      break;
+   
+    }else{
+     
+      continue;
+   
+    }
+  
+  }
+  
+  return $array;
+  
+ }
