c41c3c87b5f16f199236e89bfac76cf9da66639b
Imad Chabounia <jokerdz44@gmail.com>
2018-08-31 14:24:36 +0200

    Update SelectionSort.php
    It's a sorting algorithm of order O(n*n) in average case

--- Algorithms/Sorting/SelectionSort.php
+++ Algorithms/Sorting/SelectionSort.php
@@ -1,6 +1,28 @@
 <?php
 
-function SelectionSort($array, $n): array {
+function swap(&$a, &$b){
+  $temp = $a;
+  $a    = $b;
+  $b    = $temp;
+}
 
+function SelectionSort($array, $n): array {
   
+  for($i = 0; $i < $n-1; $i++){
+   
+    $min_i = $i;
+    
+    for($j = $i + 1; $j < $n; $j++){
+     
+      if($array[$j] < $array[$min_i]){
+        $min_i = $j;
+      
+      }
+    }
+     
+      swap($array[$i], $array[$min_i]);
+  
+  }
+  
+  return $array;
 }
