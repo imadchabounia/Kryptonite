53a27728bc9599b9eef2ca0a2924140aa1977c42
Imad Chabounia <jokerdz44@gmail.com>
2018-09-04 09:35:03 +0200

    Update StockMaximize.php

--- HackerRank/Algorithms/Dynamic Programming/StockMaximize.php
+++ HackerRank/Algorithms/Dynamic Programming/StockMaximize.php
@@ -1,1 +1,19 @@
 <?php
+
+function stockmax($prices){
+
+    $gained = 0;
+    $max = 0;
+
+    for($i = count($prices)-1; $i > -1; $i--){
+        
+        if($prices[$i] >+ $max){
+
+            $max = $prices[$i];
+        }
+
+        $gained += ($max - $prices[$i]);
+    }
+
+    return $gained;
+}
