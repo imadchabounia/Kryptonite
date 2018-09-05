5c1330a7c357d1479efebdb2ab57c4c1ba1f3368
Imad Chabounia <jokerdz44@gmail.com>
2018-09-05 15:02:50 +0200

    Update Candies.php

--- HackerRank/Algorithms/Greedy/Candies.php
+++ HackerRank/Algorithms/Greedy/Candies.php
@@ -1,2 +1,34 @@
 <?php
 
+
+function candies($n, $arr){
+  
+  $total = 0;
+  $candies = [];
+  $base = 1;
+  
+  if($n == count($arr)){
+     
+    for($i = 0; $i < $n; $i++){
+      $candies[$i] = $base;
+    }
+    
+    for($i = 1; $i < $n; $i++){ // going from left to right (asc)
+      if($arr[$i] > $arr[$i-1]){
+        $candies[$i] += $candies[$i-1];
+      }
+    }
+    
+    for($i = $n-2; $i >= 0; $i--){ // going from right to left (desc)
+      if($arr[$i] > $arr[$i+1] && $candies[$i] < $candies[$i+1]+1){
+        $candies[$i] = $candies[$i+1]+1;
+      }
+    }
+    
+    for($i = 0; $i < $n; $i++){
+      $total += $candies[$i];
+    }
+    
+    return $total;
+  }
+}
