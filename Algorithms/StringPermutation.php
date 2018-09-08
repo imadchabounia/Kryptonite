6dc6d2c9b7a74c153a360fd1ca7e6d9749ba5a31
Imad Chabounia <jokerdz44@gmail.com>
2018-09-08 10:39:51 +0100

    Update StringPermutation.php

--- Algorithms/StringPermutation.php
+++ Algorithms/StringPermutation.php
@@ -1,4 +1,25 @@
 <?php
 
-// this work also with arrays as strings
+// it works also with arrays
+
+function swap($string, $left, $right){
+  $temp = $string[$left];
+  $string[$left] = $string[$right];
+  $string[$right] = $temp;
+  
+  return $string;
+}
+
+//This problem will be solved Recursivly
+
+function permute($string, $left, $right){
+  if($left == $right){
+    print($string."\n");
+  }else{
+    for($i = $left; $i < $right; $i++){
+      $string = swap($string, $left, $i);
+      permute($string, $left+1, $right);
+      }
+  }
+}
 
