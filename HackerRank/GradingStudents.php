36b74a2427491c457be4e8892bb6ecc38d1b88ca
Imad Chabounia <jokerdz44@gmail.com>
2018-09-14 16:11:58 +0100

    Update GradingStudents.php

--- HackerRank/GradingStudents.php
+++ HackerRank/GradingStudents.php
@@ -1,2 +1,13 @@
 <?php
 
+function gradingStudents($grades) {
+
+for($i = 0; $i < count($grades); $i++){
+    if((((($grades[$i]-$grades[$i]%5)+5)-$grades[$i]) < 3) && (($grades[$i]-$grades[$i]%5)+5) >= 40){
+        $grades[$i] = (($grades[$i]-$grades[$i]%5)+5);
+    }
+}
+
+return $grades;
+
+}
