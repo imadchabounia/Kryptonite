5e1240e767f2a2501638fb53ea01f55de1a27572
Imad Chabounia <jokerdz44@gmail.com>
2018-11-17 13:49:49 +0100

    Update MigratoryBirds.php

--- HackerRank/MigratoryBirds.php
+++ HackerRank/MigratoryBirds.php
@@ -1,2 +1,21 @@
 <?php
 
+function migratoryBirds($arr){
+
+  $freq = [0, 0, 0, 0, 0];
+  $max = 0;
+  $selected_id = 1;
+  
+  for($i = 0; $i < count($arr); $i++){
+    $freq[$arr[$i]-1] += 1;
+    
+    if($freq[$arr[$i]-1] > $max){
+    $max = $freq[$arr[$i]-1];
+      $selected_id = $arr[$i];
+    }
+    
+    
+  }
+  
+  return $selected_id;
+}
