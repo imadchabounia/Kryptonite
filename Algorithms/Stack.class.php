00a41ea046eca165bd112e37286114810182c8aa
Imad Chabounia <jokerdz44@gmail.com>
2019-02-01 14:46:30 +0000

    Create Stack.class.php
    Simple implementation of Stack Data Structure in PHP

---
+++ Algorithms/Stack.class.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Implementaion of Stack Data Structure in PHP
+ */
+interface ds
+{
+  public function push($data);
+  public function pop();
+  public function isEmpty();
+  public function top();
+
+}
+
+class Stack implements ds{
+
+  //properties
+  private $top = -1;
+  private $space = [];
+  private $object;
+  //methods
+  public function push($data)
+  {
+    $this->top = $this->top + 1;
+    $this->space[$this->top] = $data;
+
+    return;
+
+  }
+
+  public function pop()
+  {
+    $this->top = $this->top - 1;
+  }
+
+  public function isEmpty()
+  {
+    if($this->top == -1){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  public function top()
+  {
+    return $this->space[$this->top];
+  }
+
+}
