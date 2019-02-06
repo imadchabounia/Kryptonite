29038dade342fe62a92f513f52408e8b20a30d87
Imad Chabounia <jokerdz44@gmail.com>
2019-02-06 17:59:18 +0100

    Create foo.php
    recursion vs loop

---
+++ p_patterns/foo.php
@@ -0,0 +1,50 @@
+<?php
+
+print("
+/**************************************************
+[ + ]      Coded by Imad ^________-
+***************************************************/
+");
+
+function triangle1($string){
+
+  $begin = 0;
+  $end = strlen($string);
+
+  if(strlen($string) == 1){
+    print("\t \n".$string);
+    return;
+  }
+
+  triangle1(control($string, $begin+1, $end-1));
+  print("\t \n".$string);
+
+}
+
+function control($string, $start, $end){
+  $result = "";
+  for($i = $start; $i <= $end; $i++){
+
+    $result = $result.$string[$i];
+  }
+
+  return $result;
+}
+
+function triangle2($string){
+  $begin = 0;
+  $end = strlen($string)-1;
+  $i = 0;
+  $str = $string;
+  while(strlen($str) != 1){
+      $str = control($string, $begin+$i, $end);
+      $i++;
+      print("\n");
+      print("\t\t\t".$str);
+  }
+
+}
+$string = "***********************";
+
+triangle1($string);
+triangle2($string);
