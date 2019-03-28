640d4f7b7b7491b0a1f3558ed57fb8963b7dc2cf
Imad Chabounia <jokerdz44@gmail.com>
2019-03-28 21:58:50 +0100

    Create cycle_detection.c

---
+++ HackerRank/Data_Structures/Linked_Lists/cycle_detection.c
@@ -0,0 +1,16 @@
+ /*
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+/* solution using Floyd's Cycle Detection Algorithm
+bool solveHelper(struct SinglyLinkedListNode *tortoise, struct SinglyLinkedListNode *hare) {
+  if (hare == tortoise)
+    return true;
+  if (hare == NULL || tortoise == NULL || hare->next == NULL)
+    return false;
+  return solveHelper(tortoise->next, hare->next->next);
+}
+bool has_cycle(struct SinglyLinkedListNode *head) { return solveHelper(head->next, head->next->next);}
